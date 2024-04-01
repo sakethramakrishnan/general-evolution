5867f61d189e1a7c19686d2fa363e94f1b6b05fb
sakethramakrishnan <saketh.ramakrishnan@gmail.com>
2024-04-01 16:19:23 -0400

    explaination

--- general_evolution/evolution/recommend.py
+++ general_evolution/evolution/recommend.py
@@ -2,7 +2,7 @@ import os
 from amis import reconstruct_multi_models, top_n_occurrences
 import os
 from argparse import ArgumentParser
-from dataclasses import asdict, dataclass, field
+from dataclasses import dataclass, field
 
 import yaml
 
